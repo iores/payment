cb1b1757a761ddc093bf8493dba423256f6417c6
feng <657966747@qq.com>
2017-12-25 00:21:16 +0800

    处理依赖

--- framework/framework-core/src/main/java/com/itpay/core/base/dto/UuidBaseDto.java
+++ framework/framework-core/src/main/java/com/itpay/core/base/dto/UuidBaseDto.java
@@ -1,5 +1,6 @@
 package com.itpay.core.base.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.springframework.util.StringUtils;
 
 import java.util.UUID;
@@ -11,6 +12,7 @@ public class UuidBaseDto extends  BaseDto {
 
 
     @Override
+    @JsonIgnore
     public String getIdentity() {
         if(StringUtils.isEmpty(this.identity)) {
             this.identity = UUID.randomUUID().toString();
--- itpay-common/src/main/java/com/itpay/mp/user/dto/UserDto.java
+++ itpay-common/src/main/java/com/itpay/mp/user/dto/UserDto.java
@@ -4,9 +4,7 @@ import com.itpay.base.enums.ESexType;
 import com.itpay.base.enums.EUserStatus;
 import com.itpay.core.base.dto.UuidBaseDto;
 
-import java.io.Serializable;
 import java.util.Date;
-
 public class UserDto extends UuidBaseDto {
     private static final long serialVersionUID = -2172611121783144604L;
     /**
