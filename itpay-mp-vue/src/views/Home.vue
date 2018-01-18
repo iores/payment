5719460ab95fb9532dc4203eb1766b62935fc453
feng <657966747@qq.com>
2018-01-18 22:44:20 +0800

    解决主页bug 1.图标没对齐 2.菜单最小化之后再次放大，菜单只显示一半

--- itpay-mp-vue/src/views/Home.vue
+++ itpay-mp-vue/src/views/Home.vue
@@ -235,10 +235,20 @@
 				flex:0 0 60px;
 				width: 60px;
 			}
-			.menu-expanded{
-				flex:0 0 230px;
-				width: 230px;
-			}
+			/*.menu-expanded{*/
+				/*flex:0 0 230px;*/
+				/*width: 230px;*/
+			/*}*/
+            .menu-expanded {
+                flex: 0 0 230px;
+                width: 230px;
+                .el-menu {
+                    width: 100% !important;
+                }
+                .el-submenu .el-menu-item {
+                    min-width: 0px !important;
+                }
+            }
 			.content-container {
 				// background: #f1f2f7;
 				flex:1;
