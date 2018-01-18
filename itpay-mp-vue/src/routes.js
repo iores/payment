36a8f67727dedd91e04e978cb312f0b8ee21f8bb
feng <657966747@qq.com>
2018-01-18 23:52:45 +0800

    菜单处理

--- itpay-mp-vue/src/routes.js
+++ itpay-mp-vue/src/routes.js
@@ -23,14 +23,13 @@ let routes = [
         name: '',
         hidden: true
     },
-    //{ path: '/main', component: Main },
     {
         path: '/',
         component: Home,
         name: '导航一',
         iconCls: 'fa el-icon-message',//图标样式class
         children: [
-            { path: '/main', component: Main, name: '主页', hidden: true },
+            { path: '/main', component: Main, name: '主页', hidden: false },
             { path: '/table', component: Table, name: 'Table' },
             { path: '/form', component: Form, name: 'Form' },
             { path: '/user', component: user, name: '列表' },
@@ -39,21 +38,27 @@ let routes = [
     {
         path: '/',
         component: Home,
-        name: '导航二',
+        name: '个人信息',
         iconCls: 'fa fa-id-card-o',
         children: [
             { path: '/page4', component: Page4, name: '页面4' },
-            { path: '/page5', component: Page5, name: '页面5' }
+            { path: '/page5', component: Page5, name: '页面5' },
+            { path: '/page6', component: Page5, name: '我的消息' },
+            { path: '/page7', component: Page5, name: '代办事项' },
+            { path: '/page8', component: Page5, name: '我的流程' },
         ]
     },
     {
         path: '/',
         component: Home,
-        name: '',
-        iconCls: 'fa fa-address-card',
-        leaf: true,//只有一个节点
+        name: '系统管理',
+        iconCls: 'fa el-icon-setting',
+        leaf: false,//只有一个节点
         children: [
-            { path: '/page6', component: Page6, name: '导航三' }
+            { path: '/page9', component: Page6, name: '登录日志' },
+            { path: '/page10', component: Page6, name: '批处理计划' },
+            { path: '/page11', component: Page6, name: '时钟列表' },
+            { path: '/page12', component: Page6, name: '系统参数' },
         ]
     },
     {
@@ -61,6 +66,7 @@ let routes = [
         component: Home,
         name: 'Charts',
         iconCls: 'fa fa-bar-chart',
+        leaf: true,//只有一个节点
         children: [
             { path: '/echarts', component: echarts, name: 'echarts' }
         ]
